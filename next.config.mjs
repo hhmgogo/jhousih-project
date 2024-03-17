454b89d63fe49cc2c51d2421c7fb8896f551b387
hhmgogo <notingmuch999@gmail.com>
2024-03-17 22:01:27 +0800

    chang

--- next.config.mjs
+++ next.config.mjs
@@ -6,12 +6,21 @@
 
 // export default nextConfig;
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: {
+//     export: true,
+//   },
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+// next.config.mjs
+
+export default {
   output: {
-    export: true,
+    dir: "out", // 將輸出目錄設置為 'out'
   },
   reactStrictMode: true,
 };
-
-export default nextConfig;
