9a07c9c1bffa01b371812fdbbc85c56493477da1
hhmgogo <notingmuch999@gmail.com>
2024-03-17 22:17:57 +0800

    11

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,5 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: "export",
-//   reactStrictMode: true,
-// };
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: {
-    export: true,
-  },
   reactStrictMode: true,
 };
 
