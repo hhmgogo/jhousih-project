1d90d1db6ea119a6b41ae6f9214621c98e872212
hhmgogo <notingmuch999@gmail.com>
2024-03-19 22:20:31 +0800

    dd

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === "production";
-
+const assetPrefix =
+  process.env.NODE_ENV === "production" ? "/jhousih-project" : "";
 const nextConfig = {
   output: isProd ? "export" : undefined,
   reactStrictMode: true,
-  assetPrefix: isProd
-    ? "https:///hhmgogo.github.io/jhousih-project/"
-    : undefined,
+  // assetPrefix: isProd
+  //   ? "https:///hhmgogo.github.io/jhousih-project/"
+  //   : undefined,
   basePath: "/jhousih-project",
   assetPrefix: "/jhousih-project/",
 };
