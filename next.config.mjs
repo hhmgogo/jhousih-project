9f0fef2297ab185a731e4bbee353c2813bcf7236
hhmgogo <notingmuch999@gmail.com>
2024-03-21 23:27:34 +0800

    666

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,20 @@
-// /** @type {import('next').NextConfig} */
-
-// const isProd = process.env.NODE_ENV === "production";
-
-// const nextConfig = {
-//   output: isProd ? "export" : undefined,
-//   reactStrictMode: true,
-//   assetPrefix: isProd
-//     ? "https:///hhmgogo.github.io/jhousih-project/"
-//     : undefined,
-// };
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  output: "export", // Outputs a Single-Page Application (SPA).
+  output: isProd ? "export" : undefined,
+  reactStrictMode: true,
+  assetPrefix: isProd
+    ? "https:///hhmgogo.github.io/jhousih-project/"
+    : undefined,
   distDir: "./out", // Changes the build output directory to `./dist/`.
 };
-
 export default nextConfig;
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export", // Outputs a Single-Page Application (SPA).
+//   distDir: "./out", // Changes the build output directory to `./dist/`.
+// };
+
+// export default nextConfig;
