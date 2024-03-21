6371aaaec5fb92e955d1b50c8cff563d9100b709
hhmgogo <notingmuch999@gmail.com>
2024-03-21 22:19:04 +0800

    1111

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,25 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+
+// const isProd = process.env.NODE_ENV === "production";
+
+// const nextConfig = {
+//   output: isProd ? "export" : undefined,
+//   reactStrictMode: true,
+//   assetPrefix: isProd
+//     ? "https:///hhmgogo.github.io/jhousih-project/"
+//     : undefined,
+// };
+// export default nextConfig;
+
 const nextConfig = {
-  output: "export",
-  reactStrictMode: true,
-  assetPrefix: "https:///hhmgogo.github.io/jhousih-project/",
+  // Other Next.js configuration options...
+
+  // 添加 output: export
+  output: {
+    // 添加 export 選項
+    export: {
+      // 在此處添加任何進一步的選項或配置
+    },
+  },
 };
 export default nextConfig;
